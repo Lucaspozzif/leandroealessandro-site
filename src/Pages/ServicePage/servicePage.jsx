cb5a2c27891fa2504383d3b1e0dab83f275b35c9
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2022-08-18 15:25:01 -0300

    service page minor changes

--- src/Pages/ServicePage/servicePage.jsx
+++ src/Pages/ServicePage/servicePage.jsx
@@ -1,7 +1,30 @@
 import { useParams } from 'react-router-dom';
-export function ServicePage(){
-    let {professional} = useParams();
-    return(
-        <p> bom dia {professional}</p>
+import { BottomButtons } from '../../Content/bottomButtons/bottomButtons';
+import { ReadProfessional } from '../../Content/Firebase/firestoreData';
+import { ItemListServ } from '../../Content/itemList/itemList';
+
+export function ServicePage() {
+    var data = ReadProfessional()
+    const { profIndex } = useParams();
+    const service = data
+    //
+    if (data.length == 0) {
+        return console.log('vazio')
+    }
+    return (
+        <>
+            {data[profIndex]['services'].map((serv) => {
+                return (
+                    <ItemListServ
+                        key={data[profIndex]['services'].indexOf(serv)}
+                        msg={serv}
+                        profIndex={profIndex}
+                        servIndex={data[profIndex]['services'].indexOf(serv)}
+                    />
+                )
+            })}
+            <BottomButtons />
+        </>
     )
+
 }
