53d9ac772257c79b01bf613c19b16604d90f6400
Lucas Pozzi <lucaspozzif20@gmail.com>
2022-09-15 12:34:34 -0300

    itemList delete option created

--- src/Content/itemList/itemList.jsx
+++ src/Content/itemList/itemList.jsx
@@ -1,6 +1,11 @@
+import { doc, setDoc } from 'firebase/firestore'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../../Style/reset.css'
 import '../../Style/style.css'
+import { db } from '../Firebase/firebase'
+import { ReadProfessional } from '../Firebase/firestoreDataReader'
+import { saveDatabase } from '../SaveDatabase/saveDatabase'
 import './itemList.css'
 
 const { time } = require('../../Data/hourCount/hourcount.json')
@@ -63,19 +68,49 @@ export function ItemListTime(props) {
     }
 }
 export function ItemListTimeSecret(props) {
-    if(props.msg['taken'] == true){
-        return(
-            
-            <div className='greyButton scheduleButton'>
-                <h1>{time[props.index]}</h1>
-                <p>{props.msg['name']} agendou {props.msg['service']}</p>
+    const [taken,setTaken] = useState(true)
+    var data = ReadProfessional()
+    const params = props.data
+    var prof = data[params[0]]
+    if (data.length == 0) return
+
+    const addSchedule = async () => {
+        const profRef = doc(db, 'professionals', data[params[0]]['id'])
+        const update = {
+            name: prof['name'],
+            services: prof['services'],
+            occupation: prof['occupation'],
+            schedule: prof['schedule']
+        }
+        await setDoc(profRef, update)
+    }
+
+    if (props.msg['taken'] == true && taken==true) {
+        return (
+
+            <div className='greyButton scheduleButton editableButton'>
+                <div>
+                    <h1>{time[props.index]}</h1>
+                    <p>{props.msg['name']} agendou {props.msg['service']}</p>
+                </div>
+                <img
+                    onClick={() => {
+                        data[params[0]]['schedule'][params[1]][props.index]['taken'] = false
+                        addSchedule()
+                        console.log(prof['schedule'])
+                        setTaken(false)
+
+                    }}
+                    className={'deleteIcon'}
+                    src={require('../../Assets/Icons/delete.png')}
+                />
             </div>
         )
     }
     return (
-            <div className='button scheduleButton'>
-                <h1>{time[props.index]}</h1>
+        <div className='button scheduleButton'>
+            <h1>{time[props.index]}</h1>
 
-            </div>
+        </div>
     )
 }
